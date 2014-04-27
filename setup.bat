8d8b4649b7622cb01e4aebe369e98c4fbb4300ed
Claudio Mattera <claudio@mattera.it>
2014-04-27 14:02:51 +0200

    Building dependencies for tests in setup script.

--- setup.bat
+++ setup.bat
@@ -4,7 +4,7 @@ echo Initializing sandbox
 cabal sandbox init
 
 echo Installing dependencies
-cabal install --only-dependencies
+cabal install --only-dependencies --enable-tests
 
 echo Configuring Project
 cabal configure --enable-tests
